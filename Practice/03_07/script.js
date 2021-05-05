29b8f340d239dfb4c25bd6bff579b74c89a42647
abdulkaleem254 <80502220+abdulkaleem254@users.noreply.github.com>
2021-05-05 16:15:22 +0530

    Update script.js

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -1,9 +1,10 @@
-/**
- * Practice: Building objects
- *
- * - Create JavaScript objects based on objects in your current environment.
- * - Give each object an identifiable name.
- * - Create properties to describe the objects and set their values.
- * - Find an object that has another object inside of it to create a nested object.
- * - Test your objects in the browser console by accessing the entire object and its specific properties.
- */
+book{
+  page:25,
+    company:"canvas",
+      paper:"oil paper",
+        rate{
+          extrapage:60,
+            nrmlpage:30,
+        }
+    }
+  console.log(book.page)
